df471e1127a95104889d3e8b2fadf46ecc3eb59f
Jan Pechanec <jp@devnull.cz>
2020-03-21 14:27:39 +0100

    STEF now supports configure/unconfigure, and supports STEF_EXECUTABLE_LOCAL_VARS.

--- stef.sh
+++ stef.sh
@@ -7,12 +7,14 @@
 
 typeset -i fail=0
 typeset -i untested=0
+typeset -i ret
 typeset diffout=stef-diff.out
 typeset testnames
 typeset testfiles
 typeset output=stef-output-file.data
 typeset tests
 typeset LS=/bin/ls
+typeset STEF_CONFIG=stef-config
 
 typeset -i STEF_UNSUPPORTED=100
 typeset -i STEF_UNTESTED=101
@@ -38,9 +40,54 @@ fi
 
 # If you want to use variables defined in here in the tests, export them in
 # there.
-[[ -f stef-config ]] && . ./stef-config
+[[ -f $STEF_CONFIG ]] && source ./$STEF_CONFIG
 
-[[ -n "$STEF_TESTSUITE_NAME" ]] && printf "=== [ $STEF_TESTSUITE_NAME ] ===\n\n"
+[[ -n "$STEF_TESTSUITE_NAME" ]] && printf "=== [ $STEF_TESTSUITE_NAME ] ===\n"
+
+# If this variable is set, the test suite clone specific settings may put in
+# that file.  For example, stuff that each user will need to set differently,
+# like mysh, myed, or mytar binaries.
+if [[ -n $STEF_CONFIG_LOCAL && -f $STEF_CONFIG_LOCAL ]]; then
+	echo "Sourcing test suite specific configuration: $STEF_CONFIG_LOCAL"
+	source ./$STEF_CONFIG_LOCAL
+fi
+
+for var in $STEF_EXECUTABLE_LOCAL_VARS; do
+	echo "Checking test suite specific executables:" \
+	    "$STEF_EXECUTABLE_LOCAL_VARS"
+	varexec=$(eval echo \$$var)
+	[[ -x $varexec && $varexec == /* ]] && continue
+
+	if [[ $varexec != /* ]]; then
+		printf "%s\n%s\n" \
+		    "Variable '$var' set as '$varexec' in STEF_EXECUTABLE_VARS" \
+		    "variable defined in '$STEF_CONFIG' must be an absolute path."
+		echo "Please fix it before trying to re-run.  Exiting."
+		exit 1
+	fi
+
+	printf "%s\n%s\n" \
+	    "Variable '$var' set as '$varexec' in STEF_EXECUTABLE_VARS" \
+	    "variable defined in '$STEF_CONFIG' does not point to an executable."
+	echo "Please fix it before trying to re-run.  Exiting."
+	exit 1
+done
+
+if [[ -n $STEF_CONFIGURE ]]; then
+	echo "Configuring the test run..."
+	if [[ ! -x $STEF_CONFIGURE ]]; then
+		echo "Error: $STEF_CONFIGURE not executable."
+		ret=1
+	else
+		$STEF_CONFIGURE
+		ret=$?
+	fi
+	if ((ret != 0)); then
+		echo "Configuration failed, fix it and rerun.  Exiting."
+		exit 1
+	fi
+	echo "Configuration done."
+fi
 
 # Test must match a pattern "test-*.sh".  All other scripts are ignored.
 # E.g. test-001.sh, test-002.sh, test-cmd-003, etc.
@@ -51,7 +98,7 @@ if (( $# > 0 )); then
 		[[ -x test-$i.sh ]] || \
 		    { echo "$i not a valid test.  Exiting." && exit 1; }
 	done
-else 
+else
 	testfiles=$( $LS test-*.sh )
 	if (( $? != 0 )); then
 		echo "No valid tests present.  Exiting."
@@ -60,9 +107,12 @@ else
 	testnames=$( echo "$testfiles" | cut -f2- -d- | cut -f1 -d. )
 fi
 
+echo "Running tests."
+printf -- "------------\n"
+
 for i in $testnames; do
 	# Print the test number.
-	printf "$i\t"
+	printf "  $i\t"
 
 	./test-$i.sh >$output 2>&1
 	ret=$?
@@ -116,7 +166,7 @@ for i in $testnames; do
 	rm -f $output $diffout
 done
 
-printf "\n============\n\n"
+printf -- "------------\n"
 
 if [[ $fail -eq 1 ]]; then
 	echo "WARNING: some tests FAILED !!!"
@@ -127,10 +177,27 @@ if [[ $untested -eq 1 ]]; then
 	retval=1
 fi
 if [[ $fail -eq 0 && $untested -eq 0 ]]; then
-	echo "TESTS PASSED"
+	echo "All tests passed."
 	retval=0
 fi
 
-echo ""
+if [[ -n $STEF_UNCONFIGURE ]]; then
+	if ((ret != 0)); then
+		echo "Skipping unconfiguration due to some test failures."
+	else
+		echo "Unconfiguring the test run..."
+		if [[ ! -x $STEF_UNCONFIGURE ]]; then
+			echo "Error: $STEF_UNCONFIGURE not executable."
+			ret=1
+		else
+			$STEF_UNCONFIGURE
+		fi
+		if ((ret != 0)); then
+			echo "WARNING: Unconfiguration failed."
+		else
+			echo "Unconfiguration done."
+		fi
+	fi
+fi
 
 exit $retval
